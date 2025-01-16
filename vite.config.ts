262eb9df155dc5e3ec517e2c839b9cb5bac77995
Vladimir Revyagin <skorabeyf@gmail.com>
2025-01-16 13:55:26 +0300

    feat(PWA): Подключил PWA plagin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      outDir: 'build',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Note app with PWA',
+        short_name: 'Note',
+        description: 'test pwa app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './public/assets/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './public/assets/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
